2911e22a3b0ac201d32fde54459c6657da57c3f8
airplayx <airplayx@gmail.com>
2020-01-08 16:50:30 +0800

    update

--- app/sql2struct/init.go
+++ app/sql2struct/init.go
@@ -9,54 +9,41 @@ import (
 	_app "gormat/app"
 	"gormat/controllers/Sql2struct"
 	"strings"
-	"time"
 )
 
 func Screen(win fyne.Window) *fyne.Container {
-	result := widget.NewMultiLineEntry()
-	var tables []fyne.CanvasObject
-	tbs := widget.NewTabContainer()
-	for _, v := range Sql2struct.Tables {
-		tbs.Items = append(tbs.Items, widget.NewTabItemWithIcon(v.Name, _app.Table, widget.NewMultiLineEntry()))
-		tName := v.Name
-		tables = append(tables, widget.NewButton(tName, func() {
-			//if checked {
-			if rs, err := sql2struct(win, []string{tName}); err != nil {
-				result.SetText(err.Error())
+	resultBox := widget.NewMultiLineEntry()
+	tables := widget.NewTabContainer()
+	for _, t := range Sql2struct.Tables {
+		tableName := t.Name
+		tableBox := widget.NewMultiLineEntry()
+		tableBox.SetText(tableName) //读取表结构
+		tableBox.OnCursorChanged = func() {
+			if rs, err := sql2struct(win, []string{tableName}); err != nil {
+				resultBox.SetText(err.Error())
 			} else {
-				result.SetText(strings.ReplaceAll(string(rs), "\t", "    "))
+				resultBox.SetText(strings.ReplaceAll(string(rs), "\t", "    "))
 			}
-			//}
-		}))
+			resultBox.Refresh()
+		}
+		tables.Items = append(tables.Items, widget.NewTabItemWithIcon(t.Name, _app.Table, tableBox))
 	}
 	tableBox := widget.NewGroupWithScroller("表结构")
-	if len(tbs.Items) > 0 {
-		tbs.SetTabLocation(widget.TabLocationLeading)
-		tableBox.Append(tbs)
+	if len(tables.Items) > 0 {
+		tables.SetTabLocation(widget.TabLocationLeading)
+		tableBox.Append(tables)
 	}
 	return fyne.NewContainerWithLayout(
 		layout.NewGridLayout(1),
 		tableBox,
-		widget.NewScrollContainer(result),
+		widget.NewScrollContainer(resultBox),
 	)
 }
 
 func sql2struct(win fyne.Window, ts []string) (result []byte, err error) {
-	status := dialog.NewProgress("转换中...", strings.Join(ts, "\n"), win)
-	go func() {
-		num := 0.0
-		for num < 1.0 {
-			time.Sleep(50 * time.Millisecond)
-			status.SetValue(num)
-			num += 0.01
-		}
-		status.SetValue(1)
-	}()
-	status.Show()
 	if result, err = Sql2struct.NewGenTool().Gen(ts); err != nil {
 		dialog.ShowError(errors.New(err.Error()), win)
 		return
 	}
-	status.Hide()
 	return
 }
