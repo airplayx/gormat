8c40e64885c8767d325dbdec3031c53790c8163a
airplayx <airplayx@gmail.com>
2020-01-11 23:44:35 +0800

    update

--- build.bat
+++ build.bat
@@ -2,9 +2,4 @@ SET CGO_ENABLED=1
 SET CC=x86_64-w64-mingw32-gcc
 SET GOOS=windows
 SET GOARCH=amd64
-go build -o main_x64.exe -ldflags="-H windowsgui"
-
-SET CGO_ENABLED=0
-SET GOOS=linux
-SET GOARCH=amd64
-go build
+go build -o gormat.exe -ldflags="-H windowsgui"
