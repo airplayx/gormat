12081e041acc69b8704b329eb58fc17bb0f4cf81
airplayx <airplayx@gmail.com>
2020-06-01 13:10:15 +0800

    📦 compressed and packed files

--- cmd/build.bat
+++ cmd/build.bat
@@ -1,5 +1,7 @@
+::fyne package
 SET CGO_ENABLED=1
 SET CC=x86_64-w64-mingw32-gcc
 SET GOOS=windows
 SET GOARCH=amd64
-go build -o gormat.exe -ldflags="-H windowsgui"
+go build -ldflags "-s -w -X 'configs.buildTime=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%' -H windowsgui" -o gormat.exe
+upx --best gormat.exe
